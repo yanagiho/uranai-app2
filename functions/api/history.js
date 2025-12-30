e50251e8bf7d11ec6f0d4750ef814b50ed265244
yanagiho <yanagiho@gade.jp>
2025-12-30 17:06:13 +0900

    Update history.js

--- functions/api/history.js
+++ functions/api/history.js
@@ -3,10 +3,7 @@ export async function onRequestGet(context) {
   const url = new URL(request.url);
   const userId = url.searchParams.get("userId");
 
-  if (!userId) return new Response("UserId required", { status: 400 });
-
   try {
-    // ログインしているユーザーIDに一致する履歴だけを取得
     const { results } = await env.DB.prepare(`
       SELECT cast_name, card_name, card_image, datetime(created_at, 'unixepoch', 'localtime') as time 
       FROM ChatLogs 
