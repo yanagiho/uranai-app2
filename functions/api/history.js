7509ed94d3046e59b039587d1f3798bd33c9afb5
yanagiho <yanagiho@gade.jp>
2025-12-30 16:55:23 +0900

    Update history.js

--- functions/api/history.js
+++ functions/api/history.js
@@ -1,9 +1,9 @@
 export async function onRequestGet(context) {
   const { env } = context;
   try {
-    // カードを引いた時の履歴だけを抽出する
+    // カード画像が保存されている履歴のみを抽出
     const { results } = await env.DB.prepare(`
-      SELECT cast_name, card_name, card_image, content, datetime(created_at, 'unixepoch', 'localtime') as time 
+      SELECT cast_name, card_name, card_image, datetime(created_at, 'unixepoch', 'localtime') as time 
       FROM ChatLogs 
       WHERE card_name IS NOT NULL 
       ORDER BY created_at DESC 
