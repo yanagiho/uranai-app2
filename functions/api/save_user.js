af2ac0446f24d769dc9360f6362d42eb8348e3ae
yanagiho <yanagiho@gade.jp>
2026-01-02 18:41:14 +0900

    Update save_user.js

--- functions/api/save_user.js
+++ functions/api/save_user.js
@@ -1,16 +1,17 @@
 export async function onRequestPost(context) {
   const { request, env } = context;
   try {
-    const { userId, lastName, firstName, dob } = await request.json();
+    const { userId, lastName, firstName, dob, auth_type } = await request.json();
 
     await env.DB.prepare(`
-      INSERT INTO Users (id, last_name, first_name, dob, ticket_balance) 
-      VALUES (?, ?, ?, ?, 10)
+      INSERT INTO Users (id, last_name, first_name, dob, auth_type, ticket_balance) 
+      VALUES (?, ?, ?, ?, ?, 10)
       ON CONFLICT(id) DO UPDATE SET 
         last_name = excluded.last_name, 
         first_name = excluded.first_name, 
-        dob = excluded.dob
-    `).bind(userId, lastName, firstName, dob).run();
+        dob = excluded.dob,
+        auth_type = excluded.auth_type
+    `).bind(userId, lastName, firstName, dob, auth_type).run();
 
     return new Response(JSON.stringify({ success: true }));
   } catch (e) {
